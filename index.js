a73614468e2137912d57931359f5215b03ab4f61
Vitor Alex <vitoroliveirace@gmail.com>
2023-12-31 20:09:40 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let heroName = "Felipão Boladão";
+let XP = 100200;
+
+if (XP <= 1000) {
+    console.log("O Herói de nome" + heroName + "está no nível de Ferro");
+} else if (XP === 1001 || XP <= 2000) {
+    console.log("O Herói de nome" + " " + heroName + " " + "está no nível Bronze");
+} else if (XP === 2001 || XP <= 5000) {
+    console.log("O Herói de nome" + " " + heroName + " " + "está no nível Prata");
+} else if (XP === 5001 || XP <= 7000) {
+    console.log("O Herói de nome" + " " + heroName + " " + "está no nível Ouro");
+} else if (XP === 7001 || XP <= 8000) {
+    console.log("O Herói de nome" + " " + heroName + " " + "está no nível Platina");
+} else if (XP === 8001 || XP <= 9000) {
+    console.log("O Herói de nome" + " " + heroName + " " + "está no nível Ascendente");
+} else if (XP === 9001 || XP <= 10000) {
+    console.log("O Herói de nome" + " " + heroName + " " + "está no nível Imortal");
+} else if (XP >= 10001) {
+    console.log("O Herói de nome" + " " + heroName + " " + "está no nível Radiante");
+}
+
+// Felipão, arruma um trampo noix!!!
